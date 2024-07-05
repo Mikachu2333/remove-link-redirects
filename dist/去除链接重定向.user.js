ad507b3802feae1e994eec107e248060b866c551
zhangfz <2633915958@qq.com>
2024-07-05 14:31:59 +0800

    chore: Optimize package size and remove unused dependencies

--- dist/去除链接重定向.user.js
+++ dist/去除链接重定向.user.js
@@ -4,7 +4,7 @@
 // @description       去除网页内链接的重定向，具有高准确性和高稳定性，以及相比同类插件更低的时间占用，平均时间在0.02ms~0.05ms之间
 // @version           1.9.7
 // @namespace         Violentmonkey Scripts
-// @update            2024-07-05 14:29:42
+// @update            2024-07-05 14:31:54
 // @grant             GM.xmlhttpRequest
 // @match             *://www.baidu.com/*
 // @match             *://tieba.baidu.com/*
