4d48b36e66a11d06ee0a16a827464ff835dfe135
zhangfz <2633915958@qq.com>
2024-08-16 08:56:12 +0800

    添加对NodeSeek的适配

--- remove-link-redirects.js
+++ remove-link-redirects.js
@@ -2,7 +2,7 @@
 // @name              去除链接重定向
 // @author            Meriel
 // @description       能原地解析的链接绝不在后台访问，去除重定向的过程快速且高效，平均时间在0.02ms~0.05ms之间。几乎没有任何在后台访问网页获取去重链接的操作，一切都在原地进行，对速度精益求精。去除网页内链接的重定向，具有高准确性和高稳定性，以及相比同类插件更低的时间占用。并且保证去除重定向的有效性，采用三级方案，原地解析->自动跳转->后台访问，保证了一定能去除重定向链接
-// @version           2.5.8
+// @version           2.5.9
 // @namespace         Violentmonkey Scripts
 // @grant             GM.xmlHttpRequest
 // @match             *://*/*
@@ -186,6 +186,15 @@
           );
         },
       },
+      {
+        name: "NodeSeek",
+        urlTest: /www\.nodeseek\.com\/jump\?to=(.*)/,
+        resolveAutoJump: function () {
+          location.href = decodeURIComponent(
+            new URL(location.href).searchParams.get("to")
+          );
+        },
+      },
     ];
   }
 
@@ -310,7 +319,7 @@
     static removeLinkRedirect(caller, element, realUrl, options) {
       element.setAttribute(RedirectApp.REDIRECT_COMPLETED, "true");
       if ((realUrl && element.href !== realUrl) || options?.force) {
-        element.href = realUrl;
+        element.href = decodeURIComponent(realUrl);
       } else if (caller) {
         if (!caller.fallbackResolver) {
           caller.fallbackResolver = new RedirectApp.FallbackResolver();
@@ -1182,6 +1191,18 @@
           if (a) {
             RedirectApp.removeLinkRedirect(this, element, a.href);
           }
+        },
+      },
+      {
+        name: "NodeSeek",
+        urlTest: /www\.nodeseek\.com/,
+        linkTest: /www\.nodeseek\.com\/jump\?to=(.*)/,
+        resolveRedirect: function (element) {
+          RedirectApp.removeLinkRedirect(
+            this,
+            element,
+            new URL(element.href).searchParams.get("to")
+          );
         },
       },
     ];
