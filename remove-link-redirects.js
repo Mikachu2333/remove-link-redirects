3849e8ad351bb9691b084f0b9830f8c184ffc869
zhangfz <2633915958@qq.com>
2024-11-18 16:12:47 +0800

    修复google搜索时右键点击链接导致重定向失效的问题

--- remove-link-redirects.js
+++ remove-link-redirects.js
@@ -2,7 +2,7 @@
 // @name              去除链接重定向
 // @author            Meriel
 // @description       能原地解析的链接绝不在后台访问，去除重定向的过程快速且高效，平均时间在0.02ms~0.05ms之间。几乎没有任何在后台访问网页获取去重链接的操作，一切都在原地进行，对速度精益求精。去除网页内链接的重定向，具有高准确性和高稳定性，以及相比同类插件更低的时间占用。并且保证去除重定向的有效性，采用三级方案，原地解析->自动跳转->后台访问，保证了一定能去除重定向链接
-// @version           2.7.6
+// @version           2.7.7
 // @namespace         Violentmonkey Scripts
 // @grant             GM.xmlHttpRequest
 // @match             *://*/*
@@ -980,13 +980,15 @@
             const realUrl = element.getAttribute("data-href");
             RedirectApp.removeLinkRedirect(this, element, realUrl);
           }
-          const url = new URL(element.href);
-          if (url.searchParams.get("url")) {
-            RedirectApp.removeLinkRedirect(
-              this,
-              element,
-              url.searchParams.get("url")
-            );
+          if (element && element.href) {
+            const url = new URL(element?.href);
+            if (url?.searchParams.get("url")) {
+              RedirectApp.removeLinkRedirect(
+                this,
+                element,
+                url.searchParams.get("url")
+              );
+            }
           }
         },
       },
@@ -1272,7 +1274,7 @@
         urlTest: /w+\.google\./,
         linkTest: false,
         onInit: function () {
-          document.addEventListener("mousedown", function (event) {
+          window.addEventListener("mousedown", function (event) {
             const $a = event?.target?.closest("a");
             if ($a && !$a.getAttribute(RedirectApp.REDIRECT_COMPLETED)) {
               const oldUrl = $a.href;
